dc16cd34e6093d327615eba3a9f57a9ba21577b1
muccicj <muccicj@clarkson.edu>
2022-10-03 12:49:55 -0400

    Finished ClackData

--- src/Data/ClackData.java
+++ src/Data/ClackData.java
@@ -1,4 +1,43 @@
 package Data;
 
-public class ClackData {
+import java.util.Date;
+public abstract class ClackData {
+    public final int CONSTANT_LISTUSERS = 0;
+    public final int CONSTANT_LOGOUT = 1;
+    public final int CONSTANT_SENDMESSAGE = 2;
+    public final int CONSTANT_SENDFILE = 3;
+
+
+    private String username;
+    private int type;
+    private Date date;
+
+    public void ClackData(String userName, int type){
+
+        this.username = userName;
+        this.type = type;
+        this.date = new Date();
+
+    }
+    public void ClackData (int type) {
+        this.ClackData("Anon" , type);
+    }
+
+    public void ClackData() {
+        this.ClackData("Anon", CONSTANT_LISTUSERS);
+        this.date = new Date();
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getUserName() {
+        return this.username;
+    }
+
+    public abstract Date getDate();
+
+
 }
+
