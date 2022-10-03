74da2d2a11cdfb309d7de81a7b6b027d1cba5ec4
Floyd <Floydwmcdonald@gmail.com>
2022-10-03 13:12:44 -0400

    Finished MessageClackData

--- src/Data/MessageClackData.java
+++ src/Data/MessageClackData.java
@@ -1,4 +1,26 @@
 package Data;
 
-public class MessageClackData {
+public class MessageClackData extends ClackData {
+
+    public String message;
+    MessageClackData(String userName, String message, int type) {
+        super.ClackData(userName,type);
+        this.message = message;
+    }
+
+    public String getData() {
+        return this.message;
+    }
+
+    public String toString(){
+        return "This class is called the MessageClackData class." + "\n " +
+                " It is responsible for overwriting," + "\n" +
+                " Hashcode(), equals() and this toString() method. " +"\n" +
+                "This class also sets up constructors for" + "MessageClackData." + "\n" +
+                "It also inherits from Clack Data." + "\n" +
+                "Both classes use the instance variables username, message, and type";
+
+
+
+    }
 }
