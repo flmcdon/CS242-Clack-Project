8097b09e982da191da958021fb64ec1d567313de
muccicj <muccicj@clarkson.edu>
2022-10-04 11:38:16 -0400

    Working on TestClackData

---
+++ src/test/TestClackData.java
@@ -0,0 +1,16 @@
+package test;
+
+import Data.FileClackData;
+import Data.MessageClackData;
+import Data.ClackData;
+
+public class TestClackData {
+    public void main (String[] args) {
+        MessageClackData mcd = new MessageClackData();
+        FileClackData fcd = new FileClackData();
+
+
+
+
+    }
+}
